8113037f8d1b114eafedd7c6e9c05a088336a52b
belalhassan91 <belal.abuelella@outlook.com>
2020-08-14 22:21:49 +0200

    fix Doker shell script

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 DockerTagNum="v $DockerBuildVersion"
 docker build -t captainbelal/udacity_capstone:$DockerTagNum .
-docker push captainbelal/udacity_capstone:$DockerTagNum
+sudo docker push captainbelal/udacity_capstone:$DockerTagNum
 export DockerBuildVersion=$(($DockerBuildVersion+1))
