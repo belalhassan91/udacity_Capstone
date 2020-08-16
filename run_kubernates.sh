4a412996dcb405f8dc110ce77272cfe98b60bcb6
belalhassan91 <belal.abuelella@outlook.com>
2020-08-16 20:30:52 +0200

    solve background at shell script

--- run_kubernates.sh
+++ run_kubernates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 minikube start --vm-driver=none
 kubectl create deployment udacity-capstone --image=captainbelal/udacity_capstone:86
-sleep 60
-kubectl port-forward deployment/udacity-capstone --address 0.0.0.0 80:80&
+sleep 10
+nohup kubectl port-forward deployment/udacity-capstone --address 0.0.0.0 80:80 &
